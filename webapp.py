67a03d7095cb98d4541dfc13f677e695bcd5e773
Jothish babu <jothishwp8@outlook.com>
2019-02-11 20:03:50 +0530

    new ones

--- webapp.py
+++ webapp.py
@@ -1,13 +1,13 @@
 from flask import Flask,render_template #import flask library
-app=Flask(__name__,template_folder='templates')         #name to app obj
-@app.route('/')             #first line / represents the route dir 
-def index():                 #definiton of '/' is home its is mandetery in this code 
-    return render_template('index.html')    #content of that page...
-@app.route('/about/')       #another page with that quated name we can create a page
+app=Flask(__name__)         #name to app obj
+@app.route("/")             #first line / represents the route dir 
+def home():                 #definiton of "/" is home its is mandetery in this code 
+    return render_template("home.html")    #content of that page...
+@app.route("/about/")       #another page with that quated name we can create a page
 def about():                #page definition
-    return render_template('about.html')
-@app.route('/registration/')       #another page with that quated name we can create a page
+    return render_template("about.html")
+@app.route("/registration/")       #another page with that quated name we can create a page
 def registration():                #page definition
-    return render_template('registration.html')
-if __name__ == '__main__':      #check wether the ___name__ == __main__ conforming only devoloper can debug the code... if this command is not here anyone can access and debug the code..
+    return render_template("registration.html")
+if __name__ == "__main__":      #check wether the ___name__ == __main__ conforming only devoloper can debug the code... if this command is not here anyone can access and debug the code..
     app.run() 
